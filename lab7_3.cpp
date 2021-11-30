bf2298400707d70f289cc74a68c516d0a32bbefe
FrienDDDD <friendjunk@gmail.com>
2021-11-30 18:46:47 +0700

    lab7_3.cpp complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,9 +2,52 @@
 
 using namespace std;
 
+int adiff(int A,int B) {
+  
+  while(A >= 360) {
+    A = A - 360 ; 
+  }
+  while(B >= 360) {
+    B = B - 360 ; 
+  }
+  
+  while(A < 0) {
+    A = A + 360 ; 
+  }
+  while(B < 0) {
+    B = B + 360 ; 
+  }
+
+  
+  
+  int max,min ;
+  if(A >= B) {
+    max = A ;
+    min = B ;
+  }else{
+    min = A ;
+    max = B ;
+  }
+  
+  if(max-min > 180) {
+    min = min + 360 ;
+  }
+
+
+  int C ;
+  C = max-min ; 
+  if(C < 0){
+    C = -C ;
+  }
+  
+  cout << endl ;
+  return C ;
+}
+
+
 
 int main(){
-  cout << adiff(180,270);
+  cout << adiff(180,270)  ;
   cout << adiff(210,45);
   cout << adiff(0,360);
   cout << adiff(10,350);
